944c2b8e44ef9dae57f3a77d66055bb89ea2369c
AgosHenderson <2000189@ucc.edu.ar>
2022-04-12 18:22:16 -0300

    Se agregaron cambios

---
+++ ej-mvc/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.New()
+	engine.GET("/ping, Ping")
+	engine.Run(":3000")
+}
+
+func Ping(ctx *gin.Context) {
+	ctx.String(200, "pong")
+}
